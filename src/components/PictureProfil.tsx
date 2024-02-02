e0691e2dad43f06e8f2aaaa33755d138a621f6d4
MThorel <mthorel@corp.free.fr>
2024-02-02 14:40:18 +0100

    refacto label style

--- src/components/PictureProfil.tsx
+++ src/components/PictureProfil.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
-import { Separator } from './UI/Separator'
 
 export default function ProfilPicture() {
+  const labelStyle =
+    'absolute px-2 py-[2px] pt-1 rounded-md bg-gray-primary bg-opacity-20 drop-shadow-md text-white uppercase tracking-wider text-xs duration-300 group-hover:scale-110'
+
   return (
     <div className="group relative m-4 ml-0">
       <Image
@@ -9,18 +11,18 @@ export default function ProfilPicture() {
         src="/assets/mdytrl.webp"
         width={568}
         height={824}
-        className=" drop-shadow rounded-md transition ease-in-out duration-700	saturate-0 hover:saturate-100 hover:cursor-pointer"
+        className="drop-shadow rounded-md transition ease-in-out duration-700	saturate-0 hover:saturate-100 hover:cursor-pointer"
       />
-      <span className="absolute px-2 py-[2px] rounded-md bg-gray-primary bg-opacity-20 drop-shadow-md text-white uppercase tracking-wider text-sm top-56 -left-8 group-hover:-translate-y-2 group-hover:translate-x-6 duration-300">
+      <span className={`${labelStyle} top-56 -left-8 group-hover:-translate-y-2 group-hover:translate-x-6`}>
         Créative
       </span>
-      <span className="absolute px-2 py-[2px] rounded-md bg-gray-primary bg-opacity-20 drop-shadow-md text-white uppercase tracking-wider text-sm pt-1 top-12 right-2 group-hover:translate-y-6 group-hover:-translate-x-2 duration-300">
+      <span className={`${labelStyle} top-12 right-2 group-hover:translate-y-6 group-hover:-translate-x-2`}>
         Organisée
       </span>
-      <span className="absolute px-2 py-[2px] rounded-md bg-gray-primary bg-opacity-20 drop-shadow-md text-white uppercase tracking-wider text-sm pt-1 bottom-20 left-8 group-hover:-translate-y-4 group-hover:translate-x-6 duration-300">
+      <span className={`${labelStyle} bottom-20 left-8 group-hover:-translate-y-4 group-hover:translate-x-6`}>
         Optimiste
       </span>
-      <span className="absolute px-2 py-[2px] rounded-md bg-gray-primary bg-opacity-20 drop-shadow-md text-white uppercase tracking-wider text-sm pt-1 bottom-40 right-8 group-hover:-translate-y-4 group-hover:-translate-x-6 duration-300">
+      <span className={`${labelStyle} bottom-40 right-8 group-hover:-translate-y-4 group-hover:-translate-x-6`}>
         Curieuse
       </span>
     </div>
