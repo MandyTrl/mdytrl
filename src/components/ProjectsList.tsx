5340a9debc3edee907aa6e9b967e65bfdac41281
MThorel <mthorel@corp.free.fr>
2024-02-21 13:58:28 +0100

    projectlist add new style + icons

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,18 +1,32 @@
-import { projects } from '@/utils/datas/projects'
 import Link from 'next/link'
+import Image from 'next/image'
+import { projects } from '@/utils/datas/projects'
 
 export const ProjectsList = () => {
   return (
-    <div className="w-3/5 h-full ml-6 hover:cursor-pointer">
+    <div className="w-3/5 h-full ml-8 hover:cursor-pointer">
       {projects.map((project, index) => {
         return (
-          <div key={index} className="last:border-b border-t border-primary py-6">
-            <p className="group pl-2 uppercase transform hover:font-soria hover:tracking-widest hover:scale-[1.4] duration-500 hover:translate-x-[110px]">
-              <Link href={`/projects/${project.name}`}>
-                &lt; {project.name} <span className="group-hover:text-yel-primary">/</span>
-                &gt;
-              </Link>
-            </p>
+          <div key={index} className="group last:border-b border-t border-primary py-6">
+            <Link href={`/projects/${project.name}`}>
+              <div className="flex justify-between px-2">
+                <p className="uppercase transform group-hover:scale-[1.25] duration-300 group-hover:translate-x-[10%]">
+                  &lt; {project.name} <span className="group-hover:text-yel-primary">/</span>
+                  &gt;
+                </p>
+                <p className="flex items-center">
+                  <span className="text-sm mr-4 opacity-0 group-hover:opacity-100 transform duration-300 group-hover:translate-x-[0%] translate-x-[50%]">
+                    Voir le projet
+                  </span>
+                  <Image
+                    src="/assets/icons/arrow-up-right.svg"
+                    alt={`go to the project ${project.name}`}
+                    width={15}
+                    height={15}
+                  />
+                </p>
+              </div>
+            </Link>
           </div>
         )
       })}
