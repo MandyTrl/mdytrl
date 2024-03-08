54e53a5de44123ab01de37c2f55e366a22ae06c2
MThorel <mthorel@corp.free.fr>
2024-03-08 14:50:27 +0100

    add txt

--- src/components/Layouts/AboutLayout.tsx
+++ src/components/Layouts/AboutLayout.tsx
@@ -5,6 +5,7 @@ import { CV } from '../About/CV'
 export const AboutLayout = () => {
   return (
     <div className="h-full flex flex-col items-center text-gray-primary">
+      <h4 className="text-8xl self-start mt-20">Donner forme aux idées</h4>
       <ProfilPicture />
       <Bio />
       <CV />
