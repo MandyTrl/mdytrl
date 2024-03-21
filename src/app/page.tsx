f8af0f0dcd793ab23a86cb93ae8c9f0871ec7d33
MThorel <mthorel@corp.free.fr>
2024-03-21 16:20:25 +0100

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { Intro } from '@/components/About/Intro'
 export default function Home() {
   return (
     <section id="home" className="flex flex-col items-center justify-center lg:px-14">
-      <div className="flex items-center text-[33px] lg:text-[5.7rem] uppercase">
+      <div className="flex flex-col items-center text-[33px] lg:text-[5.7rem] uppercase">
         <img src="/assets/photos/mt-berlin.webp" alt="mandy-thorel" className="lg:w-[70%]" />
 
-        {/* <h1 className="leading-9 lg:leading-none">
+        <h1 className="leading-9 lg:leading-none">
           Développeuse <br></br> Full stack
-        </h1> */}
+        </h1>
       </div>
 
       <LocalTime />
